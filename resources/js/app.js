36564ecc1cb3ee1b913ae2e32f911a53fdca3ed4
UncleMilobear <107782416+NestumMiloBertaburan@users.noreply.github.com>
2025-03-23 16:31:25 +0800

    feat: add livewire library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,4 @@
 import './bootstrap';
-
-import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 
 window.Alpine = Alpine;
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,3 +10,6 @@
 
 <!-- Scripts -->
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+<!-- Livewire -->
+@livewireStyles
